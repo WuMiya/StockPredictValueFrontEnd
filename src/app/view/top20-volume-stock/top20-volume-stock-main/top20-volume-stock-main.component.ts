9ac0513f537e9456bdaa9cc73c4f4dc6c0d5191f
Miya Wu <miya.wu0930@gmail.com>
2021-08-04 22:54:37 +0800

    update table column to meet the api resp

--- src/app/view/top20-volume-stock/top20-volume-stock-main/top20-volume-stock-main.component.ts
+++ src/app/view/top20-volume-stock/top20-volume-stock-main/top20-volume-stock-main.component.ts
@@ -8,6 +8,26 @@ import * as moment from 'moment';
 import * as _ from 'lodash';
 import 'moment/locale/zh-tw';
 
+interface StringKey {
+  [key: string]: any;
+}
+
+const ChineseColumn: StringKey = {
+  rank: '排名',
+  companyId: '證券代號',
+  companyName: '證券名稱',
+  transactionShares: '成交股數',
+  transactionAmount: '成交筆數',
+  openingPrice: '開盤價',
+  highestPrice: '最高價',
+  lowestPrice: '最低價',
+  closingPrice: '收盤價',
+  upsDowns: '漲跌(+/-)',
+  priceDifference: '漲跌價差',
+  lastRevealBuyPrice: '最後揭示買價',
+  lastRevealSealPrice: '最後揭示賣價',
+};
+
 @Component({
   selector: 'app-top20-volume-stock-main',
   templateUrl: './top20-volume-stock-main.component.html',
@@ -24,25 +44,29 @@ export class Top20VolumeStockMainComponent implements OnInit {
   ngOnInit(): void {
     moment.locale('zh-tw');
     this.date = moment().format('LL');
-
     this.getTop20VolumeStocks();
   }
 
   getTop20VolumeStocks() {
-    // const date = moment().format('YYYYMMDD');
     this.loading = true;
-    const result: Top20VloumeStockInfo[] = [];
+    const result: any[] = [];
     this.dataSvc.getTop20VolumeStocks().subscribe(
       (resp) => {
-        const keys = resp.payLoad.fields;
-        resp.payLoad.data.forEach((d, idx: number) => {
-          const obj = {} as Top20VloumeStockInfo;
-          d.forEach((value: any, i: number) => {
-            _.set(obj, keys[i], value);
-            result[idx] = obj;
-          });
+        resp.payLoad.forEach((data: StringKey) => {
+          if (data.upsDowns === 'up') {
+            data.upsDowns = '+';
+          } else if (data.upsDowns === 'down') {
+            data.upsDowns = '-';
+          }
+          for (const k in data) {
+            const newkey = ChineseColumn[k];
+            data[newkey] = data[k];
+            delete data[k];
+          }
+          result.push(data);
         });
         this.top20VolumeStocks = result;
+        const keys = Object.values(ChineseColumn);
         keys.forEach((k) => {
           this.headers.push({
             field: k,
@@ -57,5 +81,34 @@ export class Top20VolumeStockMainComponent implements OnInit {
         console.log(error);
       }
     );
+  }
+
+  getColor(val: string) {
+    switch (val) {
+      case '+':
+        return 'red';
+
+      case '-':
+        return 'green';
+
+      default:
+        return 'black';
+    }
+  }
+
+  getFontWeight(val: string) {
+    if (val === '+' || val === '-') {
+      return 'bolder';
+    } else {
+      return 'normal';
+    }
+  }
+
+  getFontSize(val: string) {
+    if (val === '+' || val === '-') {
+      return '20px';
+    } else {
+      return 'small';
+    }
   }
 }
